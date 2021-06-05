39c6eb51d9cf9334d798b0c6e8491d4a131c6995
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2021-06-05 18:41:17 -0300

    Descrição Estrutural: Somador Completo

---
+++ Descrição Estrutural/Projeto_01/Projeto_01.v
@@ -0,0 +1,13 @@
+/*
+	*Projeto01.v
+	*Descrição:SOmador completo em verilog
+*/
+module Projeto_01(input a, b, cin, output s, cout);
+	wire [2:0] x;
+	
+   xor U1(x[0],a,b);
+   xor U2(cout,cin,x[0]);
+	and U3(x[1],cin,x[0]);
+	and U4(x[2],a,b);
+	or  U5(s,x[1],x[2]);
+endmodule
