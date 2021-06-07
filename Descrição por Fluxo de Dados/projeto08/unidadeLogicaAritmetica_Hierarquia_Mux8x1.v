be5913716081c329799790dc812b07fbdc59971a
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2021-06-07 13:38:16 -0300

    ULA usando hierarquia

--- Descrição por Fluxo de Dados/projeto08/unidadeLogicaAritmetica_Hierarquia_Mux8x1.v
+++ Descrição por Fluxo de Dados/projeto08/unidadeLogicaAritmetica_Hierarquia_Mux8x1.v
@@ -1,3 +1,7 @@
+/*	Unidade Lógica-Aritmética em Verilog
+	Implemente, em FPGA e utilize o verilog, uma unidade lógica-aritmética de 4-bits que possua a tabela de operação apresentada ao lado
+	
+*/
 module unidadeLogicaAritmetica_Hierarquia_Mux8x1(input[2:0] sel,input[3:0] A,B, output [3:0] out);
 	mux8x1_4bits multiplexador8x1_4Bits(sel,A + B,A - B,A << B,A >> B,A & B, A | B,A ^ B, ~A,out);
 endmodule
